3e6e23af65f3e768154ab455ccf2f8c68863303f
krawller <ayoubk3r@gmail.com>
2024-06-21 22:55:29 +0100

    catsay-tui using cursive

---
+++ catsay-tui/src/main.rs
@@ -0,0 +1,60 @@
+use cursive::event::Key;
+use cursive::traits::Nameable;
+use cursive::views::{Checkbox, Dialog, EditView, ListView};
+use cursive::Cursive;
+
+pub(crate) struct CatSayOptions<'a> {
+    message: &'a str,
+    dead: bool,
+}
+
+fn input_step(siv: &mut Cursive) {
+    siv.add_layer(
+        Dialog::new()
+            .title("Please fill out the form for the cat")
+            .content(
+                ListView::new()
+                    .child("Message:", EditView::new().with_name("message"))
+                    .child("Dead?", Checkbox::new().with_name("dead")),
+            )
+            .button("OK", |s| {
+                let message = s
+                    .call_on_name("message", |t: &mut EditView| t.get_content())
+                    .unwrap();
+                let is_dead = s
+                    .call_on_name("dead", |t: &mut Checkbox| t.is_checked())
+                    .unwrap();
+                let options = CatSayOptions {
+                    message: &message,
+                    dead: is_dead,
+                };
+                result_step(s, &options) // [2]
+            }),
+    );
+}
+
+fn result_step(siv: &mut Cursive, options: &CatSayOptions) {
+    let eye = if options.dead { "x" } else { "o" };
+    let cat_text = format!(
+        "{msg}\\
+\\
+/\\_/\\
+( {eye} {eye} )
+=( I )=",
+        msg = options.message,
+        eye = eye
+    );
+    siv.pop_layer(); // [3]
+    siv.add_layer(
+        // [4]
+        Dialog::text(cat_text)
+            .title("The cat says...")
+            .button("OK", |s| s.quit()),
+    );
+}
+
+fn main() {
+    let mut siv = cursive::default();
+    input_step(&mut siv); // [1]
+    siv.run();
+}
